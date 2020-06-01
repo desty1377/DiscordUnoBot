a47c18792a0e112f8e3ca6bae77fa02ab2941e1b
shadeyg56 <shadeyg56@gmail.com>
2020-06-01 14:06:08 -0500

    Several changes

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ bot.db = mongo.unobot
 @bot.event
 async def on_ready():
 	print("Bot is ready")
+	bot.players = []
 
 
 @bot.command()
@@ -64,23 +65,34 @@ def decode(hand, color=False):
 	return "\n".join(new)
 	
 
-@bot.command(alias=["start", "s"])
+@bot.command(aliases=["start", "s"])
 async def startgame(ctx, *, users):
 	try:
-		users = users.split()
-		players = [ctx.author.id]
+		userslist = users.split()
+		userslist.insert(0, ctx.author.mention)
+		players = []
 		deck = makedeck()
-		for user in users:
-			if user.startswith("<@") and user.endswith(">"):
-				players.append(int(user.lstrip("<@").lstrip("!").lstrip("&").strip(">")))
 		await bot.db.games.insert_one({"_id": ctx.author.id})
+		for user in userslist:
+			if user.startswith("<@") and user.endswith(">"):
+				id = int(user.lstrip("<@").lstrip("!").lstrip("&").strip(">"))
+				if not bot.get_user(id).bot:
+					if id not in bot.players:
+						players.append(id)
+						bot.players.append(id)
+					else:
+						await ctx.send(f"{user} is already in a game so they will not be added to the game. Please wait until they are done to add them to a game.")
+				else:
+					await ctx.send(f"{user} is a bot therefore they will not be added to the game.")
+			else:
+				await ctx.send(f"{user} is not a valid user so they will not be added to the game.")
 		await bot.db.games.update_one({"_id": ctx.author.id}, {"$set": {"players": players}})
 		#dealing cards
 		await ctx.send("Dealing the cards... check your DMs")
 		first = random.choice(players)
 		for x in players:
 			hand = []
-			n = 7
+			n = 2
 			while n>0:
 				card = random.choice(deck)
 				hand.append(card)
@@ -117,8 +129,8 @@ async def startgame(ctx, *, users):
 		try:
 			msg = await bot.wait_for("message", check=lambda m: m.author == ctx.author and m.content.lower() in ["y", "n", "yes", "no"], timeout=10)
 			if msg.content.lower() == "y" or msg.content.lower() == "yes":
-				await deletegame(ctx)
-				await startgame(ctx, users)
+				await asyncio.gather(deletegame(ctx))
+				await startgame(ctx=ctx, users=users)
 			else:
 				await ctx.send("You cannot start a new game until you delete your current game. Please run u.startgame again or run u.deletegame to manually delete your game")
 		except asyncio.TimeoutError:
@@ -126,91 +138,96 @@ async def startgame(ctx, *, users):
 
 
 async def turn(id, player):
-	data = await bot.db.games.find_one({"_id": id})
-	player = bot.get_user(player)
-	notif = await player.send("It is your turn! To play a card enter the number to the left of the card. To draw a card type 'draw', if you only have 2 cards left and you play one remember to type 'uno'")
-	action = await bot.wait_for("message", check=lambda m: m.author.id == player.id)
-	if action.content.isdigit():
-		card = data[str(player.id)]["hand"][int(action.content) - 1]
-		if card[0] == data["currentcard"][0].lower() or card[1:] == data["currentcard"][1:]:
-			await bot.db.games.update_one({"_id": id}, {"$set": {"currentcard": card}})
-			hand = data[str(player.id)]["hand"]
-			hand.remove(card)
-			await bot.db.games.update_one({"_id": id}, {"$set": {f"{str(player.id)}.hand": hand}})
-			if card[1:] == "skip":
-				if data["players"].index(player.id) + 1 == len(data["players"]):
-					skipped = 0
-				else:
-					skipped = data["players"].index(player.id) + 1
-				skipped = data["players"][skipped]
-				await notif.delete()
-				await asyncio.gather(uno_check(id, player))
-				await update_embeds(id, player, f"{player.name} played a {decode([card])} on {bot.get_user(skipped).name}", skip=True)
-			elif card[1:] == "rev":
-				if data["rotation"] == "forward":
-					await bot.db.games.update_one({"_id": id}, {"$set": {"rotation": "reverse"}})
-					move = f"{player.name} played a {decode([card])} and changed the rotation to reverse"
-				else:
-					await bot.db.games.update_one({"_id": id}, {"$set": {"rotation": "forward"}})
-					move = f"{player.name} played a {decode([card])} and changed the rotation to forward"
+	try:
+		data = await bot.db.games.find_one({"_id": id})
+		player = bot.get_user(player)
+		notif = await player.send("It is your turn! To play a card enter the number to the left of the card. To draw a card type 'draw', if you only have 2 cards left and you play one remember to type 'uno'")
+		action = await bot.wait_for("message", check=lambda m: m.author.id == player.id)
+		if action.content.isdigit():
+			card = data[str(player.id)]["hand"][int(action.content) - 1]
+			if card[0] == data["currentcard"][0].lower() or card[1:] == data["currentcard"][1:]:
+				await bot.db.games.update_one({"_id": id}, {"$set": {"currentcard": card}})
+				hand = data[str(player.id)]["hand"]
+				hand.remove(card)
+				await bot.db.games.update_one({"_id": id}, {"$set": {f"{str(player.id)}.hand": hand}})
+				if card[1:] == "skip":
+					if data["players"].index(player.id) + 1 == len(data["players"]):
+						skipped = 0
+					else:
+						skipped = data["players"].index(player.id) + 1
+					skipped = data["players"][skipped]
 					await notif.delete()
-				await asyncio.gather(uno_check(id, player))
-				await update_embeds(id, player, move)
-			elif card[1:] == "+2":
-				if data["players"].index(player.id) + 1 == len(data["players"]):
-					victim = 0
+					await asyncio.gather(uno_check(id, player))
+					await update_embeds(id, player, f"{player.name} played a {decode([card])} on {bot.get_user(skipped).name}", skip=True)
+				elif card[1:] == "rev":
+					if data["rotation"] == "forward":
+						await bot.db.games.update_one({"_id": id}, {"$set": {"rotation": "reverse"}})
+						move = f"{player.name} played a {decode([card])} and changed the rotation to reverse"
+					else:
+						await bot.db.games.update_one({"_id": id}, {"$set": {"rotation": "forward"}})
+						move = f"{player.name} played a {decode([card])} and changed the rotation to forward"
+						await notif.delete()
+					await asyncio.gather(uno_check(id, player))
+					await update_embeds(id, player, move)
+				elif card[1:] == "+2":
+					if data["players"].index(player.id) + 1 == len(data["players"]):
+						victim = 0
+					else:
+						victim = data["players"].index(player.id) + 1
+					victim = data["players"][victim]
+					await draw(id, victim, 2)
+					await notif.delete()
+					await asyncio.gather(uno_check(id, player))
+					await update_embeds(id, player, f"{player.name} played a {decode([card])} on {bot.get_user(victim).name}", skip=True)
 				else:
-					victim = data["players"].index(player.id) + 1
-				victim = data["players"][victim]
-				await draw(id, victim, 2)
-				await notif.delete()
-				await asyncio.gather(uno_check(id, player))
-				await update_embeds(id, player, f"{player.name} played a {decode([card])} on {bot.get_user(victim).name}", skip=True)
-			else:
-				move = f"{player.name} played a {decode([card])}"
-				await notif.delete()
-				await asyncio.gather(uno_check(id, player))
-				await update_embeds(id, player, move)
-		elif "wild" in card:
-			await player.send("You played a wild. Please type one of the four colors to change to.")
-			choice = await bot.wait_for("message", check=lambda m: m.author.id == player.id and m.content.lower() in ["blue", "red", "green", "yellow"])
-			if "wild+4" in card:
-				colors = {"blue": "Blue Wild +4", "red": "Red Wild +4", "yellow": "Yellow Wild +4", "green": "Green Wild +4"}
-			else:
-				colors = {"blue": "Blue Wild", "red": "Red Wild", "yellow": "Yellow Wild", "green": "Green Wild"}
-			hand = data[str(player.id)]["hand"]
-			hand.remove(card)
-			await bot.db.games.update_one({"_id": id}, {"$set": {f"{str(player.id)}.hand": hand}})
-			await bot.db.games.update_one({"_id": id}, {"$set": {"currentcard": colors[choice.content]}})
-			if card == "wild+4":
-				if data["players"].index(player.id) + 1 == len(data["players"]):
-					victim = 0
+					move = f"{player.name} played a {decode([card])}"
+					await notif.delete()
+					await asyncio.gather(uno_check(id, player))
+					await update_embeds(id, player, move)
+			elif "wild" in card:
+				await player.send("You played a wild. Please type one of the four colors to change to.")
+				choice = await bot.wait_for("message", check=lambda m: m.author.id == player.id and m.content.lower() in ["blue", "red", "green", "yellow"])
+				if "wild+4" in card:
+					colors = {"blue": "Blue Wild +4", "red": "Red Wild +4", "yellow": "Yellow Wild +4", "green": "Green Wild +4"}
 				else:
-					victim = data["players"].index(player.id) + 1
-				victim = data["players"][victim]
-				await draw(id, victim, 4)
-				await asyncio.gather(uno_check(id, player))
-				await update_embeds(id, player, f"{player.name} used a wild +4 on {bot.get_user(victim).name} and changed the color to {choice.content}", skip=True)
+					colors = {"blue": "Blue Wild", "red": "Red Wild", "yellow": "Yellow Wild", "green": "Green Wild"}
+				hand = data[str(player.id)]["hand"]
+				hand.remove(card)
+				await bot.db.games.update_one({"_id": id}, {"$set": {f"{str(player.id)}.hand": hand}})
+				await bot.db.games.update_one({"_id": id}, {"$set": {"currentcard": colors[choice.content]}})
+				if card == "wild+4":
+					if data["players"].index(player.id) + 1 == len(data["players"]):
+						victim = 0
+					else:
+						victim = data["players"].index(player.id) + 1
+					victim = data["players"][victim]
+					await draw(id, victim, 4)
+					await asyncio.gather(uno_check(id, player))
+					await update_embeds(id, player, f"{player.name} used a wild +4 on {bot.get_user(victim).name} and changed the color to {choice.content}", skip=True)
+				else:
+					await asyncio.gather(uno_check(id, player))
+					await update_embeds(id, player, f"{player.name} used a wild and changed the color to {choice.content}")
 			else:
-				await asyncio.gather(uno_check(id, player))
-				await update_embeds(id, player, f"{player.name} used a wild and changed the color to {choice.content}")
+				await player.send("The card you play must match the current card in either color or numeric value. Please try again.")
+				await notif.delete()
+				await turn(id, player.id)
+		elif action.content.lower() == "draw":
+			await draw(id, player.id)
+			await notif.delete()
+			await update_embeds(id, player, f"{player.name} drew a card.")
 		else:
-			await player.send("The card you play must match the current card in either color or numeric value. Please try again.")
+			await player.send("Invalid move! Please try again.")
 			await notif.delete()
 			await turn(id, player.id)
-	elif action.content.lower() == "draw":
-		await draw(id, player.id)
-		await notif.delete()
-		await update_embeds(id, player, f"{player.name} drew a card.")
-	else:
-		await player.send("Invalid move! Please try again.")
-		await notif.delete()
-		await turn(id, player.id)
+	except KeyError:
+		return
 
 async def draw(id, player, num=1):
 	data = await bot.db.games.find_one({"_id": id})
+	deck = data["deck"]
 	while num>0:
-		deck = data["deck"]
+		if len(deck) == 0:
+			deck = makedeck()
 		card = random.choice(deck)
 		deck.remove(card)
 		hand = data[str(player)]["hand"]
@@ -237,65 +254,80 @@ async def uno_check(id, player):
 
 async def update_embeds(id, play, action, next=True, skip=False):
 	data = await bot.db.games.find_one({"_id": id})
-	for player in data["players"]:
-		player = bot.get_user(int(player))
-		if next==True:
-			if not skip:
-				if data["players"].index(play.id) + 1 == len(data["players"]):
-					currentplayer = 0
-				else:
-					currentplayer = data["players"].index(play.id) + 1
-			else:
-				if len(data["players"]) > 2:
-					if data["players"].index(play.id) + 2 >= len(data["players"]):
-							currentplayer = 1
+	if len(data[str(play.id)]["hand"]) ==  0:
+		for player in data["players"]:
+			await bot.get_user(player).send(f"{play.name} played their last card and won the game! Game over... deleting your game now")
+			for player in data["players"]:
+				if player in bot.players:
+					bot.players.remove(player)
+			await bot.db.games.delete_one({"_id": ctx.author.id})
+	else:
+		for player in data["players"]:
+			player = bot.get_user(int(player))
+			await player.trigger_typing()
+			if next==True:
+				if not skip:
+					if data["players"].index(play.id) + 1 == len(data["players"]):
+						currentplayer = 0
 					else:
-						currentplayer = data["players"].index(play.id) + 2
+						currentplayer = data["players"].index(play.id) + 1
 				else:
-					currentplayer = data["players"].index(play.id)
-		else:
-			currentplayer = data["players"].index(play.id)
-		if data["rotation"] == "forward":
-			if len(data["players"]) - 1 == currentplayer:
-				nextplayer=0
+					if len(data["players"]) > 2:
+						if data["players"].index(play.id) + 2 >= len(data["players"]):
+								currentplayer = 1
+						else:
+							currentplayer = data["players"].index(play.id) + 2
+					else:
+						currentplayer = data["players"].index(play.id)
 			else:
-				nextplayer = currentplayer + 1
-		else:
-			if currentplayer == 0:
-				nextplayer = len(data["players"]) - 1
+				currentplayer = data["players"].index(play.id)
+			if data["rotation"] == "forward":
+				if len(data["players"]) - 1 == currentplayer:
+					nextplayer=0
+				else:
+					nextplayer = currentplayer + 1
 			else:
-				nextplayer = currentplayer - 1
-		if "Wild" in data["currentcard"]:
-			if "Wild +4" in data["currentcard"]:
-				cardpic = discord.File(f"assets/wild+4.png", filename="image.png")
+				if currentplayer == 0:
+					nextplayer = len(data["players"]) - 1
+				else:
+					nextplayer = currentplayer - 1
+			if "Wild" in data["currentcard"]:
+				if "Wild +4" in data["currentcard"]:
+					cardpic = discord.File(f"assets/wild+4.png", filename="image.png")
+				else:
+					cardpic = discord.File(f"assets/wild.png", filename="image.png")
+				currentcard = data["currentcard"]
 			else:
-				cardpic = discord.File(f"assets/wild.png", filename="image.png")
-			currentcard = data["currentcard"]
-		else:
-			cardpic = discord.File(f"assets/{data['currentcard']}.png", filename="image.png")
-			currentcard = decode([data["currentcard"]])
-		currentplayer = bot.get_user(data['players'][currentplayer])
-		nextplayer = bot.get_user(data["players"][nextplayer])
-		embed = discord.Embed(title="Uno Game Info", description=f"{action}\n{currentplayer.name}'s turn\n{nextplayer.name}'s turn is next", color=decode([data["currentcard"]], color=True))
-		embed.set_thumbnail(url="attachment://image.png")
-		embed.add_field(name="Current Card", value=currentcard)
-		embed.add_field(name="Your Hand", value=decode(data[str(player.id)]["hand"]))
-		embed.add_field(name=f"{bot.get_user(int(data['turn'])).name}'s Hand", value=f"{len(data[str(currentplayer.id)]['hand'])} cards", inline=True)
-		embed.add_field(name="Rotation of play", value=data["rotation"], inline=True)
-		msg = await player.fetch_message(int(data[str(player.id)]["msg"]))
-		await msg.delete()
-		await asyncio.sleep(1.5)
-		msg = await player.send(file=cardpic, embed=embed)
-		await bot.db.games.update_one({"_id": id}, {"$set": {f"{str(player.id)}.msg": msg.id}})
-	if next==True:
-		await turn(id, currentplayer.id)
+				cardpic = discord.File(f"assets/{data['currentcard']}.png", filename="image.png")
+				currentcard = decode([data["currentcard"]])
+			currentplayer = bot.get_user(data['players'][currentplayer])
+			nextplayer = bot.get_user(data["players"][nextplayer])
+			embed = discord.Embed(title="Uno Game Info", description=f"{action}\n{currentplayer.name}'s turn\n{nextplayer.name}'s turn is next", color=decode([data["currentcard"]], color=True))
+			embed.set_thumbnail(url="attachment://image.png")
+			embed.add_field(name="Current Card", value=currentcard)
+			embed.add_field(name="Your Hand", value=decode(data[str(player.id)]["hand"]))
+			embed.add_field(name=f"{bot.get_user(int(data['turn'])).name}'s Hand", value=f"{len(data[str(currentplayer.id)]['hand'])} cards", inline=True)
+			embed.add_field(name="Rotation of play", value=data["rotation"], inline=True)
+			msg = await player.fetch_message(int(data[str(player.id)]["msg"]))
+			await msg.delete()
+			await asyncio.sleep(1.5)
+			msg = await player.send(file=cardpic, embed=embed)
+			await bot.db.games.update_one({"_id": id}, {"$set": {f"{str(player.id)}.msg": msg.id}})
+		if next==True:
+			await turn(id, currentplayer.id)
 
 
 @bot.command()
 async def deletegame(ctx):
+	data = await bot.db.games.find_one({"_id": ctx.author.id})
+	try:
+		for player in data["players"]:
+			if player in bot.players:
+				bot.players.remove(player)
+	except KeyError:
+		pass
 	await bot.db.games.delete_one({"_id": ctx.author.id})
 	await ctx.send("Your UNO game was deleted")
-
 			
 @bot.command()
 @commands.is_owner()
